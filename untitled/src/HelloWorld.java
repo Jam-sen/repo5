134b768160f7812983a9b20813e7c3f2cc31594f
Jam-sen <478767276@qq.com>
2020-08-25 15:39:44 +0800

    123

---
+++ untitled/src/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println ("HellpWorld1");
+        System.out.println ("HellpWorld2");
+        System.out.println ("HellpWorld3");
+        System.out.println ("HellpWorld4");
+    }
+}

52096cc1ec7d5be046c1307274517c277c5270a3
Jam-sen <478767276@qq.com>
2020-08-25 15:39:00 +0800

    delete

--- untitled/src/HelloWorld.java
+++
@@ -1,8 +0,0 @@
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println ("HellpWorld1");
-        System.out.println ("HellpWorld2");
-        System.out.println ("HellpWorld3");
-        System.out.println ("HellpWorld4");
-    }
-}
